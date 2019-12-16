15919631ed81c2d081145d01fdf8b1cc9528ed24
moosejaw <joshdemir@gmail.com>
2019-12-16 23:15:12 +0000

    Forward stderr output to devnull

--- docker/attacker/scripts/frontend.py
+++ docker/attacker/scripts/frontend.py
@@ -37,7 +37,8 @@ def runSlowloris():
         '5',
         'http://target/'
     ]
-    proc     = subprocess.Popen(commands, stdout=open(os.devnull))
+    proc     = subprocess.Popen(commands, stdout=open(os.devnull),
+        stderr=open(os.devnull))
     proc.communicate()
     time.sleep(TIME_TO_RUN)
     proc.kill()
