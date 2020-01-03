5dad2d848b0a878d59194bb1ae215ae58414843e
moosejaw <joshdemir@gmail.com>
2020-01-03 17:46:20 +0000

    Build the graphs and PRs

--- graph_analysis.py
+++ graph_analysis.py
@@ -13,6 +13,10 @@ from threading import Thread
 import modules.common_functions
 from modules.terminal_colour import Color
 
+# Pyspark modules
+from pyspark.sql import SparkSession, DataFrame
+from graphframes import GraphFrame
+
 COLOUR      = Color()
 BASE_DIR    = 'output'
 PR_DIR      = os.path.join(BASE_DIR, 'pageranks')
@@ -21,15 +25,18 @@ GRAPH_DIRS  = ['source_ip', 'destination_ip']
 SAMPLING_ITERATIONS = 5
 SAMPLE_SIZE = 5000
 SEEDS       = [i for i in range(SAMPLING_ITERATIONS)]
+GRAPH_DATA  = {k: [] for k in GRAPH_DIRS}
+
+# Create the SparkSession
+SPARK       = SparkSession.builder.appName('PacketRouteGraph').getOrCreate()
 
 def getSampleFromFile(file, seed):
     '''Returns a sample dataframe from a given file.'''
     return pd.read_csv(file, encoding='latin', \
-        usecols=['source_ip', 'source_port', 'destination_ip',
-        'destination_port'])\
+        usecols=['source_ip', 'destination_ip'])\
         .sample(n=SAMPLE_SIZE, random_state=seed)
 
-def generateGraphs(pr_file, data_files):
+def countRoutes(pr_file, data_files):
     # Start by loading the contents of the pagerank file into a list
     ips = []
     with open(pr_file, 'r') as f:
@@ -54,10 +61,6 @@ def generateGraphs(pr_file, data_files):
         for seed in SEEDS:
             # Load the sample into a dataframe
             df = getSampleFromFile(file, seed)
-            # Quickly convert port numbers back to ints
-            # (avoids some overlap down the road)
-            df['source_port'] = df['source_port'].astype(int)
-            df['destination_port'] = df['destination_port'].astype(int)
 
             # Now, for each IP address returned from the PageRank script,
             # we'll count how many times each unique route occurs with the
@@ -96,7 +99,77 @@ def generateGraphs(pr_file, data_files):
         pp.pprint(top)
         COLOUR.reset()
 
+        # Now we'll add these routes and their counts to the GRAPH_DATA variable
+        # based on the column they belong to
+        GRAPH_DATA[column].append(top)
+
+def generateGraphs():
+    '''
+    This function takes the top routes found in the countRoutes function,
+    for the source_ip and dest_ip columns and creates a graph of each one.
+
+    Ultimately it will generate a list of the most significant target and
+    attack IPs and Ports.
+
+    This function utilises the built-in PageRank algorithm from GraphX to
+    determine the significance of each route.
+    '''
+    for column_key in GRAPH_DATA.keys():
+        print(f'\nGenerating graph for column {column_key}...')
+        # Start by concatenating the route counts into one dict object
+        routes = {}
+        for dict in GRAPH_DATA[column_key]:
+            for key in dict.keys():
+                routes[key] = dict[key]
+
+        # Now we'll extract each vertex (unique IP address) from the routes,
+        # and we'll take the number of connections as the weight for the edge.
+        #
+        # We'll also have to assign an ID for each IP address and build an array
+        # of edges where each route is represented with its corresponding weight
+        verts = []
+        edges = []
+        ids   = {}
+        id    = 1
+        for key in routes.keys():
+            weight = routes[key] # Get the weight from the dict value
+            ips_from_route = (key.split('/')[1], key.split('/')[2])
+            for ip in ips_from_route:
+                # Create a new ID for the IP address if it hasn't appeared yet
+                if not ip in [i[1] for i in verts]:
+                    verts.append((id, ip)) # Add it to the vertex array too
+                    ids[ip] = id
+                    id += 1 # And increment the ID
+            # Now get the route and add it to the edges array as a tuple of:
+            # (src, dest, weight)
+            edges.append((ids[ips_from_route[0]], ids[ips_from_route[1]],
+                weight))
+
+        # Next, we'll convert the verts and edges arrays into Spark dataframes
+        # so that we can load them into GraphFrames
+        verts = SPARK.createDataFrame(verts, ['id', 'ip_address'])
+        edges = SPARK.createDataFrame(edges, ['src', 'dest', 'wgt'])
+
+        # Then we'll create the GraphFrames object, look at the edges, verts
+        # and degrees, then run PageRank.
+        graph = GraphFrame(verts, edges)
+        print('Graph vertices:')
+        graph.vertices.show()
+        print('Graph edges:')
+        graph.edges.show()
+        print('Graph degrees:')
+        graph.degrees.show()
+
+        pr = graph.pageRank(resetProbability=0.15, tol=0.01)
+        print('Graph PageRank vertices:')
+        pr.vertices.show()
+        print('Graph PageRank edges:')
+        pr.edges.show()
+
 if __name__ == '__main__':
+    # Turn off logs from pyspark to clean up output
+    SPARK.context.setLogLevel('ERROR')
+
     # First, begin by creating new folders for the graph outputs
     try:
         os.mkdir(OUTPUT_DIR)
@@ -126,7 +199,10 @@ if __name__ == '__main__':
     # dataset.
     threads = []
     for pr_file in pagerank_files:
-        proc = Thread(target=generateGraphs, args=[pr_file, data_files])
+        proc = Thread(target=countRoutes, args=[pr_file, data_files])
         threads.append(proc)
         proc.start()
     for thread in threads: thread.join()
+
+    # Now move onto create and display the actual graphs
+    generateGraphs()
