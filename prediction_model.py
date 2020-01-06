7d1e11cca5345cd2e161c9cec8a14caa7d915066
moosejaw <joshdemir@gmail.com>
2020-01-06 17:31:15 +0000

    More tweaking

--- prediction_model.py
+++ prediction_model.py
@@ -61,10 +61,10 @@ if __name__ == '__main__':
     for file in files:
        for chunk in pd.read_csv(file, chunksize=CHUNK_SIZE, usecols=columns):
         chunk = chunk.reindex(columns=sorted(chunk.columns))
-        feats = row.drop(columns=['label'].to_numpy().tolist())
-        feats = [float(i) for i in feats]
         rdd = sc.parallelize([Row(label=row['label'],
-            features=Vectors.dense(feats)) for i, row in chunk.iterrows()])
+            features=Vectors.dense([float(e) for e in row.drop(\
+            columns=['label'].to_numpy().tolist())])) for i, row in \
+            chunk.iterrows()]) # Really ugly but no better way of doing it!
         data = sc.union([data, rdd])
 
     # Now for the k-fold validaton and creating the model.
