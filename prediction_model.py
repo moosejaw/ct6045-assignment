9b7f1648190cd35d3fa62f9bfbc05fe9da5e1261
moosejaw <joshdemir@gmail.com>
2020-01-07 16:05:42 +0000

    Train a stream on the prediction model

--- prediction_model.py
+++ prediction_model.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 '''
-This scripts creates a Support Vector Machine, trained on the columns returned
-from the `feature_reduction.py` script.
+This scripts creates a Regression model, trained on the columns returned
+from the `feature_reduction.py` script in order to classify whether packets
+are BENIGN or MALICIOUS.
 '''
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.classification import SVMWithSGD
+from pyspark.mllib.classification import StreamingLogisticRegressionWithSGD
 
 import os
 import pprint
@@ -17,11 +18,13 @@ from modules.terminal_colour import Color
 COLOUR      = Color()
 OUTPUT_DIR  = 'output'
 FEATUR_DIR  = os.path.join(OUTPUT_DIR, 'features')
-COLUMNS_TO_USE_FILE = os.path.join(FEATUR_DIR, 'top_features.txt')
+COLUMNS     = 14 # All the numerical features we will use
 CHUNK_SIZE  = 4096
 SEED        = 12345
 K           = 10 # Value of k used in k-fold validation
-HDFS_FOLDER = '/user/hduser/data'
+
+CHECKPOINT_DIR = 'hdfs:///user/hduser/checkpoint'
+DATA_FOLDER    = 'hdfs:///user/hduser/data'
 
 def setupStreamContext():
     '''Sets up a new streaming context for spark.'''
@@ -45,113 +48,80 @@ if __name__ == '__main__':
     # Create the streaming context
     ssc = StreamingContext(sc).getOrCreate(CHECKPOINT_DIR, setupStreamContext)
 
-    # Start by enumerating the files in the output directory
-    files = [os.path.join(OUTPUT_DIR, i) for i in os.listdir(OUTPUT_DIR) \
-        if os.path.isfile(os.path.join(OUTPUT_DIR, i)) and i.endswith('.csv')]
+    # Read files from HDFS into stream
+    data = ssc.textFileStream(DATA_FOLDER).map(lambda line: \
+        LabeledPoint(label=line.split(',')[0],
+        features=line.split(',')[1:]))
+    training, testing = data.randomSplit([0.7, 0.3], seed=SEED)
 
-    # Load the columns we want to use into an array so we can tell pandas
-    # which columns we want to use when loading the dataframes
-    columns = []
-    with open(COLUMNS_TO_USE_FILE, 'r') as f:
-        for line in f: columns.append(line[:-1])
-    columns.append('label') # Make sure to include the label too
+    # Create the model and train it on the training dataset
+    print('Training the regression model...')
+    model = StreamingLogisticRegressionWithSGD(numIterations=10)
+    model.setInitialWeights([0 for i in range(COLUMNS)])
 
-    # Declare an RDD which will contain all of the data
-    data = sc.parallelize([])
+    # Now predict on the testing data
+    print('Predicting on the testing data...')
+    model.predictOnValues(testing).print()
 
-    # Iterate over each file in chunks, convert the chunk to a dataframe,
-    # then to a LabeledPoint and append it to the RDD containing all the data.
-    COLOUR.setBlueText()
-    print('Collecting the dataset into an RDD...')
-    COLOUR.reset()
-    for file in files:
-       for chunk in pd.read_csv(file, chunksize=CHUNK_SIZE, usecols=columns):
-        chunk = chunk.reindex(columns=sorted(chunk.columns))
-        rdd = sc.parallelize([LabeledPoint(row['label'],
-            row.drop(columns=['label']).to_numpy()) \
-            for i, row in chunk.iterrows()])
-        data = sc.union([data, rdd])
-
-    # Now for the k-fold validaton and creating the model.
-    # We'll start by random-splitting our
-    # RDD into k groups, as specified by the global varaible.
+    # Once fit, we will test it on our holdout group and record the
+    # TPs, TNs, FPs and FNs in a dict
+    # COLOUR.setBlueText()
+    # print('Checking predictions made on the holdout group...')
+    # COLOUR.reset()
+    # scorecard = {'true_pos': 0,
+    #     'false_pos': 0,
+    #     'true_neg': 0,
+    #     'false_neg': 0,
+    #     'len': 0}
     #
-    # All the splits are output into a list, so we can test the model
-    # for each split we have made - i.e. k times. The other splits will be used
-    # as the training data.
-    data = data.randomSplit(weights=[1/K for i in range(K)], seed=SEED)
-    scores = []
-
-    for group_i in range(len(data)):
-        COLOUR.setBlueText()
-        print(f'Training the model to be tested on group {group_i}...')
-        COLOUR.reset()
-
-        # We'll take splits[x] as the test data and use the rest as training
-        train_data = sc.union([data[i] for i in range(len(data)) if i != group_i])
-
-        # Declare an SVM and fit it to the training data
-        model = SVMWithSGD.train(train_data, iterations=10)
-
-        # Once fit, we will test it on our holdout group and record the
-        # TPs, TNs, FPs and FNs in a dict
-        COLOUR.setBlueText()
-        print('Checking predictions made on the holdout group...')
-        COLOUR.reset()
-        scorecard = {'true_pos': 0,
-            'false_pos': 0,
-            'true_neg': 0,
-            'false_neg': 0,
-            'len': 0}
-
-        for test_row in data[group_i].collect():
-            label = test_row.label
-            pred  = model.predict(test_row.features)
-            scorecard['len'] += 1
-
-            # Get whether the prediction was TP, FP, TN, FN
-            if label == 0.0 and label == pred:
-                scorecard['true_neg'] += 1
-            elif label == 1.0 and label == pred:
-                scorecard['true_pos'] += 1
-            elif label == 0.0 and label != pred:
-                scorecard['false_pos'] += 1
-            else:
-                scorecard['false_neg'] += 1
-
-            # Print the scorecard, append it to the scores array and move to
-            # next iteration
-            COLOUR.setBlueText()
-            print(f'The scores for iteration {group_i} are:')
-            pp = pprint.PrettyPrinter(indent=4)
-            pp.pprint(scorecard)
-            COLOUR.reset()
-            scores.append(scorecard)
-
-        # Now we'll get the sum of each score for each iteration and calculate
-        # the statistics
-        scorecard = {'true_pos': 0,
-            'false_pos': 0,
-            'true_neg': 0,
-            'false_neg': 0,
-            'len': 0}
-        for score in scores:
-            for key in score.keys():
-                scorecard[key] += score[key]
-
-        # Print the overall scores
-        COLOUR.setGreenText()
-        pp = pprint.PrettyPrinter(indent=4)
-        print('The overall test results for the model are:')
-        pp.pprint(scorecard)
-        print('\n')
-
-        # Get the stats and print them
-        acc = getAccuracy(scorecard['true_pos'] + scorecard['true_neg'],
-            scorecard['len'])
-        print(f'Accuracy    : {acc}')
-        sens = getSensitivity(scorecard['true_pos'], scorecard['false_neg'])
-        print(f'Sensitivity : {sens}')
-        spec = getSpecificity(scorecard['true_neg'], scorecard['false_pos'])
-        print(f'Specificity : {spec}')
-        COLOUR.reset()
+    # for test_row in data[group_i].collect():
+    #     label = test_row.label
+    #     pred  = model.predict(test_row.features)
+    #     scorecard['len'] += 1
+    #
+    #     # Get whether the prediction was TP, FP, TN, FN
+    #     if label == 0.0 and label == pred:
+    #         scorecard['true_neg'] += 1
+    #     elif label == 1.0 and label == pred:
+    #         scorecard['true_pos'] += 1
+    #     elif label == 0.0 and label != pred:
+    #         scorecard['false_pos'] += 1
+    #     else:
+    #         scorecard['false_neg'] += 1
+    #
+    #     # Print the scorecard, append it to the scores array and move to
+    #     # next iteration
+    #     COLOUR.setBlueText()
+    #     print(f'The scores for iteration {group_i} are:')
+    #     pp = pprint.PrettyPrinter(indent=4)
+    #     pp.pprint(scorecard)
+    #     COLOUR.reset()
+    #     scores.append(scorecard)
+    #
+    # # Now we'll get the sum of each score for each iteration and calculate
+    # # the statistics
+    # scorecard = {'true_pos': 0,
+    #     'false_pos': 0,
+    #     'true_neg': 0,
+    #     'false_neg': 0,
+    #     'len': 0}
+    # for score in scores:
+    #     for key in score.keys():
+    #         scorecard[key] += score[key]
+    #
+    # # Print the overall scores
+    # COLOUR.setGreenText()
+    # pp = pprint.PrettyPrinter(indent=4)
+    # print('The overall test results for the model are:')
+    # pp.pprint(scorecard)
+    # print('\n')
+    #
+    #     # Get the stats and print them
+    #     acc = getAccuracy(scorecard['true_pos'] + scorecard['true_neg'],
+    #         scorecard['len'])
+    #     print(f'Accuracy    : {acc}')
+    #     sens = getSensitivity(scorecard['true_pos'], scorecard['false_neg'])
+    #     print(f'Sensitivity : {sens}')
+    #     spec = getSpecificity(scorecard['true_neg'], scorecard['false_pos'])
+    #     print(f'Specificity : {spec}')
+    #     COLOUR.reset()
