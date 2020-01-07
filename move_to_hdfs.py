a60ebce186b9130d2df4117c7186cb5aa778328d
moosejaw <joshdemir@gmail.com>
2020-01-07 19:50:06 +0000

    Move each folder to hdfs

--- move_to_hdfs.py
+++ move_to_hdfs.py
@@ -11,11 +11,6 @@ HADOOP_BIN_DIR = '/usr/local/hadoop/bin'
 HDFS_DATA_DIR  = '/user/hduser/data'
 
 if __name__ == '__main__':
-    # Get files
-    files = [os.path.join(CSV_DIR, file) for file in os.listdir(CSV_DIR) \
-        if os.path.isfile(os.path.join(CSV_DIR, file)) \
-        and file.endswith('_new.csv')]
-
     # Create the data_dir in hdfs
     proc = subprocess.Popen([
         f'{HADOOP_BIN_DIR}/hdfs',
@@ -27,17 +22,16 @@ if __name__ == '__main__':
     proc.communicate()
 
     # Call a subprocess for each file, copying it across
-    for file in files:
-        hdfs_fname = file.split('/')
-        hdfs_fname = hdfs_fname[len(hdfs_fname) - 1]
-        commands = [
-            f'{HADOOP_BIN_DIR}/hdfs',
-            'dfs',
-            '-copyFromLocal',
-            f'{file}',
-            f'{HDFS_DATA_DIR}/{hdfs_fname}'
-        ]
-        proc = subprocess.Popen(commands)
-        proc.communicate()
+    commands = [
+        f'{HADOOP_BIN_DIR}/hdfs',
+        'dfs',
+        '-copyFromLocal',
+        f'{CSV_DIR}',
+        f'{HDFS_DATA_DIR}'
+    ]
+    hdfs_fname = file.split('/')
+    hdfs_fname = hdfs_fname[len(hdfs_fname) - 1]
+    proc = subprocess.Popen(commands)
+    proc.communicate()
 
-    print(f'Copied all files from {HDFS_DATA_DIR} to HDFS.')
+    print(f'Copied training and testing from {HDFS_DATA_DIR} to HDFS.')
