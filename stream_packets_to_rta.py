dc08be9c0429698932c4c5ef8ab6cee3e51d907f
moosejaw <joshdemir@gmail.com>
2020-01-10 19:58:13 +0000

    Set number of training files ot b sued

--- stream_packets_to_rta.py
+++ stream_packets_to_rta.py
@@ -143,11 +143,11 @@ if __name__ == '__main__':
                 print(f'Wrote {new_f} to staging area in HDFS.')
 
                 # Determine whether to stream training data into the model
-                # The first 2 .csv files to arrive will be used as further training data
+                # The first x .csv files to arrive will be used as further training data
                 #
                 # This is not unresaonable as the generated files tend to
-                # contain around 150,000 observations
-                if training_data_count < 15:
+                # contain many observations
+                if training_data_count < 8:
                     hdfs_final_dir = HDFS_SEC_TRAINING_DIR
                     training_data_count += 1
                 else: hdfs_final_dir = HDFS_STREAMING_DIR
@@ -161,7 +161,7 @@ if __name__ == '__main__':
                 ])
                 proc.communicate()
                 print(f'Moved {new_f} to streaming directory.')
-                if training_data_count < 15: print('This file was treated as training data.')
+                if training_data_count < 8: print('This file was treated as training data.')
 
                 # Remove the files from local after they are made
                 os.remove(new_f)
