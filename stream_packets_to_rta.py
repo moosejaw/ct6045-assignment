22bd355d04cebc4abff2a48c04407c18c05e7e65
moosejaw <joshdemir@gmail.com>
2020-01-10 00:13:33 +0000

    Remove the packet sniffer functionality as you need to be sudo for it to run

--- stream_packets_to_rta.py
+++ stream_packets_to_rta.py
@@ -12,8 +12,21 @@ from threading import Thread
 HADOOP_BIN_DIR     = '/usr/local/hadoop/bin'
 HDFS_STREAMING_DIR = '/user/hduser/data/streaming'
 
-def streamFilesToHDFS(csv_folder):
-    '''Searches for new .csv files and streams them into HDFS.'''
+if __name__ == '__main__':
+    # Get some necessary user input
+    print('Please enter the path where TCPDUMP_and_CICFlowMeter was cloned.\nFor example, this may be /home/users/you/TCPDUMP_and_CICFlowMeter if you cloned the repo to your home folder.')
+    print('Enter the path here: ')
+    repo_folder = input()
+
+    print('\nNow enter the interface you want to listen on.\nThis should be the interface beginning with "br-" listed when running ifconfig.')
+    print('Enter the interface here: ')
+    interface = input()
+
+    # Get info from user inputs
+    pcap_folder = os.path.join(repo_folder, 'pcap')
+    csv_folder  = os.path.join(repo_folder, 'csv')
+
+    # Search for new .csv files and stream them into HDFS.
     while True:
         # Delay
         time.sleep(5)
@@ -37,39 +50,3 @@ def streamFilesToHDFS(csv_folder):
 
             # Remove the files after they are made
             for file in files: os.remove(file)
-
-def runPacketSniffer(repo_folder, interface, pcap_folder):
-    '''Runs the packet sniffing script, creating the files to be streamed to
-    HDFS.'''
-    proc = subprocess.Popen([
-        f'{repo_folder}/capture_interface_pcap.sh',
-        interface,
-        pcap_folder
-    ])
-    proc.communicate()
-
-
-if __name__ == '__main__':
-    # Get some necessary user input
-    print('Please enter the path where TCPDUMP_and_CICFlowMeter was cloned.\nFor example, this may be /home/users/you/TCPDUMP_and_CICFlowMeter if you cloned the repo to your home folder.')
-    print('Enter the path here: ')
-    repo_folder = input()
-
-    print('\nNow enter the interface you want to listen on.\nThis should be the interface beginning with "br-" listed when running ifconfig.')
-    print('Enter the interface here: ')
-    interface = input()
-
-    # Get info from user inputs
-    pcap_folder = os.path.join(repo_folder, 'pcap')
-    csv_folder  = os.path.join(repo_folder, 'csv')
-
-    # Create the threads to run the sniffer and streamer simultaneously
-    threads = []
-    thr_1 = Thread(target=runPacketSniffer, args=[repo_folder, interface,
-        pcap_folder])
-    thr_2 = Thread(target=streamFilesToHDFS, args=[csv_folder])
-    threads.append(thr_1)
-    threads.append(thr_2)
-    thr_1.start()
-    thr_2.start()
-    for thread in threads: thread.join()
