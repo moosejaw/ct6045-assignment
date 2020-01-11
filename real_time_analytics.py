a0a2805982fee56248e1e50617c8e529465e941b
moosejaw <joshdemir@gmail.com>
2020-01-11 15:46:26 +0000

    Keep statistics to one script

--- real_time_analytics.py
+++ real_time_analytics.py
@@ -7,6 +7,9 @@ model to work correctly.
 import os
 import socket
 
+from queue import Queue
+from threading import Thread
+
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.mllib.regression import LabeledPoint
@@ -28,22 +31,84 @@ SRC_IP_COL     = 1
 COLUMN_INDEXES = [34, 16, 18, 7, 24, 22, 23, 29, 27, 28, 26, 81, 79, 82]
 MALICIOUS_IPS  = []
 
-MODEL_STATS    = {'true_pos': 0, 'false_pos': 0, 'true_neg': 0, 'false_neg': 0, 'total': 0}
+DATA_QUEUE  = Queue()
+MODEL_STATS = {'true_neg': 0, 'true_pos': 0, 'false_neg': 0, 'false_pos': 0}
 
-def sendStatistics(it):
-    '''Sends the predictions over a connection to a listening script, per the
-    recommendations of Spark.'''
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn_est = False
-    while not conn_est:
-        try:
-            sock.connect(('localhost', 10025))
-            conn_est = True
-        except ConnectionRefusedError: pass
+def getAccuracy(correct, total):
+    if not correct or not total: return 0
+    return correct / total
+
+def getSensitivity(tp, fn):
+    if not tp or not fn: return 0
+    return tp / (tp + fn)
+
+def getSpecificity(tn, fp):
+    if not tn or not fp: return 0
+    return tn / (tn + fp)
+
+def processQueue():
+    '''Processes the data in the queue.'''
+    while True:
+        if not DATA_QUEUE.empty():
+            pred = DATA_QUEUE.get()
+            if not pred[0] and not pred[1]:
+                MODEL_STATS['true_neg'] += 1
+            elif pred[0] and pred[1]:
+                MODEL_STATS['true_pos'] += 1
+            elif not pred[0] and pred[1]:
+                MODEL_STATS['false_pos'] += 1
+            elif pred[0] and not pred[1]:
+                MODEL_STATS['false_neg'] += 1
+            DATA_QUEUE.task_done()
+        else: time.sleep(2)
+
+def printStats():
+    '''Prints the model statistic tally every 10 seconds.'''
+    while True:
+        time.sleep(15)
+        tot = 0
+        for i in MODEL_STATS.keys(): tot = tot + MODEL_STATS[i]
+        acc = getAccuracy(MODEL_STATS["true_neg"] + MODEL_STATS["true_pos"],
+            tot)
+        sens = getSensitivity(MODEL_STATS["true_pos"], MODEL_STATS["false_neg"])
+        spec = getSpecificity(MODEL_STATS["true_neg"], MODEL_STATS["false_pos"])
+        print(f'''
+        ------------
+        Running Totals:
+        ------------
+        True Negs.  : {MODEL_STATS["true_neg"]}
+        True Pos.   : {MODEL_STATS["true_pos"]}
+        False Pos.  : {MODEL_STATS["false_pos"]}
+        False Negs. : {MODEL_STATS["false_neg"]}
+        Total       : {tot}
+        ------------
+        Stats:
+        ------------
+        Accuracy    : {acc}
+        Sensitivity : {sens}
+        Specificity : {spec}
+        \n
+        ''')
+
+def addToQueue(it):
+    '''Adds the data to be processed by the queue.'''
     for record in it:
-        data = f'{record[0]},{record[1]}X'
-        sock.sendall(data.encode())
-    sock.close()
+        DATA_QUEUE.put((record[0], record[1]))
+
+# def sendStatistics(it):
+#     '''Sends the predictions over a connection to a listening script, per the
+#     recommendations of Spark.'''
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     conn_est = False
+#     while not conn_est:
+#         try:
+#             sock.connect(('localhost', 10025))
+#             conn_est = True
+#         except ConnectionRefusedError: pass
+#     for record in it:
+#         data = f'{record[0]},{record[1]}X'
+#         sock.sendall(data.encode())
+#     sock.close()
 
 def processTrainingLine(line):
     '''Returns a labelled point for RDDs based on the .csv files of the training
@@ -63,6 +128,16 @@ def processGeneratedLine(line):
         features=[float(i) for i in line[7:82]])
 
 if __name__ == '__main__':
+    # Set up the threads
+    threads = []
+    thr     = Thread(target=processQueue)
+    threads.append(thr)
+    thr.start()
+    thr     = Thread(target=printStats)
+    threads.append(thr)
+    thr.start()
+    for thread in threads: thread.join()
+
     # Get user input first
     with open('config/malicious_ips.txt', 'r') as f:
         for line in f: MALICIOUS_IPS.append(str(line.replace('\n', '')))
@@ -85,7 +160,7 @@ if __name__ == '__main__':
 
     # Get the model to predict on values incoming in the streaming directory
     model.predictOnValues(testingStream.map(lambda lp: (lp.label, lp.features))\
-        ).foreachRDD(lambda rdd: rdd.foreachPartition(sendStatistics))
+        ).foreachRDD(lambda rdd: rdd.foreachPartition(addToQueue))
 
     # Start the stream and await manual termination
     ssc.start()
