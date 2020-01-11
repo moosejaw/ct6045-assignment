5ca159f35ea6385e75327e0ba5bd5e8c89b3cd11
moosejaw <joshdemir@gmail.com>
2020-01-11 16:03:13 +0000

    Centralise printing stats

--- real_time_analytics.py
+++ real_time_analytics.py
@@ -8,7 +8,6 @@ import os
 import time
 
 from queue import Queue
-from threading import Thread
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -30,87 +29,6 @@ LABEL_INDEX    = 83
 SRC_IP_COL     = 1
 COLUMN_INDEXES = [34, 16, 18, 7, 24, 22, 23, 29, 27, 28, 26, 81, 79, 82]
 MALICIOUS_IPS  = []
-
-DATA_QUEUE  = Queue()
-MODEL_STATS = {'true_neg': 0, 'true_pos': 0, 'false_neg': 0, 'false_pos': 0}
-
-def getAccuracy(correct, total):
-    if not correct or not total: return 0
-    return correct / total
-
-def getSensitivity(tp, fn):
-    if not tp or not fn: return 0
-    return tp / (tp + fn)
-
-def getSpecificity(tn, fp):
-    if not tn or not fp: return 0
-    return tn / (tn + fp)
-
-def processQueue():
-    '''Processes the data in the queue.'''
-    while True:
-        if not DATA_QUEUE.empty():
-            pred = DATA_QUEUE.get()
-            if not pred[0] and not pred[1]:
-                MODEL_STATS['true_neg'] += 1
-            elif pred[0] and pred[1]:
-                MODEL_STATS['true_pos'] += 1
-            elif not pred[0] and pred[1]:
-                MODEL_STATS['false_pos'] += 1
-            elif pred[0] and not pred[1]:
-                MODEL_STATS['false_neg'] += 1
-            DATA_QUEUE.task_done()
-        else:
-            printStats()
-            break
-
-def printStats():
-    '''Prints the model statistic tally every 10 seconds.'''
-    tot = 0
-    for i in MODEL_STATS.keys(): tot = tot + MODEL_STATS[i]
-    acc = getAccuracy(MODEL_STATS["true_neg"] + MODEL_STATS["true_pos"],
-        tot)
-    sens = getSensitivity(MODEL_STATS["true_pos"], MODEL_STATS["false_neg"])
-    spec = getSpecificity(MODEL_STATS["true_neg"], MODEL_STATS["false_pos"])
-    print(f'''
-    ------------
-    Running Totals:
-    ------------
-    True Negs.  : {MODEL_STATS["true_neg"]}
-    True Pos.   : {MODEL_STATS["true_pos"]}
-    False Pos.  : {MODEL_STATS["false_pos"]}
-    False Negs. : {MODEL_STATS["false_neg"]}
-    Total       : {tot}
-    ------------
-    Stats:
-    ------------
-    Accuracy    : {acc}
-    Sensitivity : {sens}
-    Specificity : {spec}
-    \n
-    ''', flush=False)
-
-def addToQueue(it):
-    '''Adds the data to be processed by the queue.'''
-    for record in it:
-        DATA_QUEUE.put((record[0], record[1]))
-    processQueue()
-
-
-# def sendStatistics(it):
-#     '''Sends the predictions over a connection to a listening script, per the
-#     recommendations of Spark.'''
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     conn_est = False
-#     while not conn_est:
-#         try:
-#             sock.connect(('localhost', 10025))
-#             conn_est = True
-#         except ConnectionRefusedError: pass
-#     for record in it:
-#         data = f'{record[0]},{record[1]}X'
-#         sock.sendall(data.encode())
-#     sock.close()
 
 def processTrainingLine(line):
     '''Returns a labelled point for RDDs based on the .csv files of the training
@@ -152,7 +70,7 @@ if __name__ == '__main__':
 
     # Get the model to predict on values incoming in the streaming directory
     model.predictOnValues(testingStream.map(lambda lp: (lp.label, lp.features))\
-        ).foreachRDD(lambda rdd: rdd.foreachPartition(addToQueue))
+        ).pprint(50)
 
     # Start the stream and await manual termination
     ssc.start()
